41d1923ec8bf8c251a9943dd7c2a4bc7c4f346bd
Maksym Moroz <maskmoroz@gmail.com>
2024-09-11 17:14:08 +0300

    added routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
+import { ActivatedRoute, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CountryComponent } from './country/country.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'home/:name', component: CountryComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
